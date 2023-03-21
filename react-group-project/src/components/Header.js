43d6d0c9cb7dd10a6156494fdd942f07a0e35f0b
Minh Bui <bqminh1806@gmail.com>
2023-03-20 19:34:14 -0700

    Add banner image

--- react-group-project/src/components/Header.js
+++ react-group-project/src/components/Header.js
@@ -3,12 +3,13 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => (
   <header>
-    <span className="icn-logo"><i className="material-icons">code</i></span>
+    <span className="icn-logo"><i className="material-icons">logo</i></span>
     <ul className="main-nav">
       <li><NavLink to="/">Home</NavLink></li>
       <li><NavLink to="/about">About</NavLink></li>
       <li><NavLink to="/mycart">My Cart</NavLink></li>
-    </ul>    
+    </ul>
+    <img class='banner' src="img/banner.jpg"></img>
   </header>
 );
 
