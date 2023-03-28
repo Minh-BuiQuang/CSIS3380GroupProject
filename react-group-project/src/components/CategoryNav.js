50b5a4c8dbe90c1267a5a5e3e96c0702cc1d39ba
Minh Bui <bqminh1806@gmail.com>
2023-03-28 16:43:49 -0700

    Update category navbar with categories from database

--- react-group-project/src/components/CategoryNav.js
+++ react-group-project/src/components/CategoryNav.js
@@ -4,9 +4,11 @@ import { Link, NavLink } from 'react-router-dom';
 const CategoryNav = ({ onNewArrivalClick }) => (
   <ul className="category-nav">
     <li><NavLink to="/new" onClick={onNewArrivalClick}>New Arrival</NavLink></li>
+    <li><NavLink to="/unisex">Unisex</NavLink></li>
     <li><NavLink to="/men">Men</NavLink></li>
     <li><NavLink to="/women">Women</NavLink></li>
-    <li><NavLink to="/kids">Kids</NavLink></li>
+    <li><NavLink to="/girls">Girls</NavLink></li>
+    <li><NavLink to="/boys">Boys</NavLink></li>
   </ul>   
 );
 
