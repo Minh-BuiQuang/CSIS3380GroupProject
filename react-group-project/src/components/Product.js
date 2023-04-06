53c20b67e47ff6641fd5b2801ae82345143706d4
jumipark1 <parkj71@student.douglascollege.ca>
2023-04-05 22:39:11 -0700

    added Products.js code

--- react-group-project/src/components/Product.js
+++ react-group-project/src/components/Product.js
@@ -2,19 +2,120 @@ import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 //Todo: Add clothing card dynamically
-const Product = () => (
-  <ul className="product-list">
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-    <li><Card/></li>
-  </ul>   
-);
+function Product() {
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+
+  const clickHandler = () => {
+    const tempData = data;
+    tempData.push(input);
+    setData(data);
+    setInput("");
+    console.log(data);
+  };
+
+
+  return (
+    <div className="Product">
+      <input
+        type="string"
+        Id ={input}
+        onChange={(e) => setInput(e.target.id)}
+      />
+      <input
+        type="text"
+        Gender ={input}
+        onChange={(e) => setInput(e.target.gender)}
+      />
+      <input
+        type="text"
+        MasterCategory ={input}
+        onChange={(e) => setInput(e.target.masterCategory)}
+      />
+      <input
+        type="text"
+        SubCategory ={input}
+        onChange={(e) => setInput(e.target.subCategory)}
+      />
+      <input
+        type="text"
+        ArticleType ={input}
+        onChange={(e) => setInput(e.target.articleType)}
+      />
+      <input
+        type="text"
+        BaseColour ={input}
+        onChange={(e) => setInput(e.target.baseColour)}
+      />
+      <input
+        type="text"
+        Season ={input}
+        onChange={(e) => setInput(e.target.season)}
+      />
+      <input
+        type="text"
+        Year ={String}
+        onChange={(e) => setInput(e.target.year)}
+      />
+      <input
+        type="text"
+        Usage ={input}
+        onChange={(e) => setInput(e.target.usage)}
+      />
+      <input
+        type="text"
+        ProductDisplayName ={input}
+        onChange={(e) => setInput(e.target.productDisplayName)}
+      />
+       <input
+        type="text"
+        Filename ={input}
+        onChange={(e) => setInput(e.target.filename)}
+      />
+       <input
+        type="text"
+        Link ={input}
+        onChange={(e) => setInput(e.target.link)}
+      />
+       <input
+        type="text"
+        Price ={input}
+        onChange={(e) => setInput(e.target.price)}
+      />
+       <input
+        type="text"
+        Stock ={input}
+        onChange={(e) => setInput(e.target.stock)}
+      />
+
+      <button onClick={clickHandler}>Add</button>
+
+      {data.length ? (
+        <ul>
+          {data.map((id,gender,masterCategory,subCategory,articleType,baseColour,season,year,usage,productDisplayName,filename,link,price,stock) => {
+            return 
+            <ul className="product-list">
+            <li><Card/>Id: {product.id}</li>
+            <li><Card/>Gender: {product.gender}</li>
+            <li><Card/>MasterCategory: {product.masterCategory}</li>
+            <li><Card/>SubCategory: {product.subCategory}</li>
+            <li><Card/>ArticleType: {product.articleType}</li>
+            <li><Card/>BaseColour: {product.baseColour}</li>
+            <li><Card/>Season: {product.season}</li>
+            <li><Card/>Year: {product.year}</li>
+            <li><Card/>Usage: {product.usage}</li>
+            <li><Card/>ProductDisplayName: {product.productDisplayName}</li>
+            <li><Card/>Filename: {product.filename}</li>
+            <li><Card/>Link: {product.link}</li>
+            <li><Card/>Price: {product.price}</li>
+            <li><Card/>Stock: {product.stock}</li>
+  </ul>;
+          })}
+        </ul>
+      ) : None}
+    </div>
+  );
+}
 
 
 //Todo: Load clothing data into card
