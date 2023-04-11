177b37a874b368df5c88703f6ea086d9807cdea5
Minh Bui <bqminh1806@gmail.com>
2023-04-11 13:33:30 -0700

    Increase item count to 6 for categories in Home

--- react-group-project/src/components/Home.js
+++ react-group-project/src/components/Home.js
@@ -22,15 +22,15 @@ export default function Home () {
       <h1 className='category-name'>New Arrival</h1>
       <Product products={products.slice(0,4)}/>     
       <h1 className='category-name'>Unisex</h1>
-      <Product products={products.filter(p => p.gender === 'Unisex').slice(0,4)}/>   
+      <Product products={products.filter(p => p.gender === 'Unisex').slice(0,6)}/>   
       <h1 className='category-name'>Men</h1>
-      <Product products={products.filter(p => p.gender === 'Men').slice(0,4)}/>
+      <Product products={products.filter(p => p.gender === 'Men').slice(0,6)}/>
       <h1 className='category-name'>Women</h1>
-      <Product products={products.filter(p => p.gender === 'Women').slice(0,4)}/>   
+      <Product products={products.filter(p => p.gender === 'Women').slice(0,6)}/>   
       <h1 className='category-name'>Girls</h1>   
-      <Product products={products.filter(p => p.gender === 'Girls').slice(0,4)}/>  
+      <Product products={products.filter(p => p.gender === 'Girls').slice(0,6)}/>  
       <h1 className='category-name'>Boys</h1>    
-      <Product products={products.filter(p => p.gender === 'Boys').slice(0,4)}/>    
+      <Product products={products.filter(p => p.gender === 'Boys').slice(0,6)}/>    
     </div>
   );
 }
