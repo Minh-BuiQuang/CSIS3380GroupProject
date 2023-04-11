5558d3a0396313e0390ffa47bee488934fac8780
Minh Bui <bqminh1806@gmail.com>
2023-04-11 15:36:12 -0700

    Update item count for new arrival category

--- react-group-project/src/components/Home.js
+++ react-group-project/src/components/Home.js
@@ -1,36 +1,36 @@
-import React from 'react';
-import CategoryNav from './CategoryNav'
-import Product from './Product'
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from "react";
+import CategoryNav from "./CategoryNav";
+import Product from "./Product";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-
-export default function Home () {        
-  const[products, setProducts] = useState([])
+export default function Home() {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    axios.get('https://csis3380-group-9-backend.onrender.com/products')
-    .then((res) => {
-      setProducts(res.data.sort((a, b) => b.year - a.year))
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  },[])
+    axios
+      .get("https://csis3380-group-9-backend.onrender.com/products")
+      .then((res) => {
+        setProducts(res.data.sort((a, b) => b.year - a.year));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="main-content home">
-      <CategoryNav/>
-      <h1 className='category-name'>New Arrival</h1>
-      <Product products={products.slice(0,4)}/>     
-      <h1 className='category-name'>Unisex</h1>
-      <Product products={products.filter(p => p.gender === 'Unisex').slice(0,6)}/>   
-      <h1 className='category-name'>Men</h1>
-      <Product products={products.filter(p => p.gender === 'Men').slice(0,6)}/>
-      <h1 className='category-name'>Women</h1>
-      <Product products={products.filter(p => p.gender === 'Women').slice(0,6)}/>   
-      <h1 className='category-name'>Girls</h1>   
-      <Product products={products.filter(p => p.gender === 'Girls').slice(0,6)}/>  
-      <h1 className='category-name'>Boys</h1>    
-      <Product products={products.filter(p => p.gender === 'Boys').slice(0,6)}/>    
+      <CategoryNav />
+      <h1 className="category-name">New Arrival</h1>
+      <Product products={products.slice(0, 6)} />
+      <h1 className="category-name">Unisex</h1>
+      <Product products={products.filter((p) => p.gender === "Unisex").slice(0, 6)} />
+      <h1 className="category-name">Men</h1>
+      <Product products={products.filter((p) => p.gender === "Men").slice(0, 6)} />
+      <h1 className="category-name">Women</h1>
+      <Product products={products.filter((p) => p.gender === "Women").slice(0, 6)} />
+      <h1 className="category-name">Girls</h1>
+      <Product products={products.filter((p) => p.gender === "Girls").slice(0, 6)} />
+      <h1 className="category-name">Boys</h1>
+      <Product products={products.filter((p) => p.gender === "Boys").slice(0, 6)} />
     </div>
   );
 }
