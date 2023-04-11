874310999884880f4e80c241ece816994d350095
jordiloo <jordansidneydunu@gmail.com>
2023-04-10 18:39:52 -0700

    Added links in about page

--- react-group-project/src/components/About.js
+++ react-group-project/src/components/About.js
@@ -4,7 +4,12 @@ const About = (props) => (
   <div className="main-content">
     <h2>{ props.title }</h2>
     <p>Welcome to ShopVibez, the ultimate online shopping destination for fashion-forward individuals looking to express themselves through their wardrobe.</p>
-    <p>Our team is dedicated to staying up-to-date with the latest fashion trends and curating a collection of the most stylish pieces. We work with trusted suppliers to ensure that we offer high-quality products that are both fashionable and functional.</p>  </div>
+    <p>Our team is dedicated to staying up-to-date with the latest fashion trends and curating a collection of the most stylish pieces. We work with trusted suppliers to ensure that we offer high-quality products that are both fashionable and functional.</p>
+    <p>For more information about our team and mission, please visit</p>
+    <p><a href="https://github.com/jordiloo">Jordan's GitHub profile</a>.</p>
+   <p><a href="https://github.com/Minh-BuiQuang">Minh's GitHub profile</a></p> 
+    <p><a href="https://github.com/jumipark1">Jumi's GitHub profile</a>.</p> 
+      </div>
 );
 
 export default About;
